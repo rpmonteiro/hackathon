4558915d441fe0f63835710e916487a651adaaef
Ric <ricpmonteiro@gmail.com>
2019-03-24 11:44:42 +0100

    Updates

---
+++ precache-manifest.32c0258ecec97ead359bd47f4b03df89.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb6a93061d7b22858e54",
+    "url": "/hackathon/static/css/main.09634ae1.chunk.css"
+  },
+  {
+    "revision": "cb6a93061d7b22858e54",
+    "url": "/hackathon/static/js/main.616a2838.chunk.js"
+  },
+  {
+    "revision": "10852034049cb3cd2679",
+    "url": "/hackathon/static/js/runtime~main.170e41e9.js"
+  },
+  {
+    "revision": "e8acb11547558de3f115",
+    "url": "/hackathon/static/js/2.4f1582b5.chunk.js"
+  },
+  {
+    "revision": "872af437542d89f180f8c40b80b17fbe",
+    "url": "/hackathon/static/media/cat-17.872af437.png"
+  },
+  {
+    "revision": "6c3e90f080c6dc47f8e384803a7362ca",
+    "url": "/hackathon/static/media/cat-3.6c3e90f0.png"
+  },
+  {
+    "revision": "bc714a7011ce3942d46910e79dbda7bb",
+    "url": "/hackathon/static/media/cat-6.bc714a70.png"
+  },
+  {
+    "revision": "90a366701c5f85658e2f8078007fba79",
+    "url": "/hackathon/static/media/cat-7.90a36670.png"
+  },
+  {
+    "revision": "f4a79852898ee04d04a974f6e17532a5",
+    "url": "/hackathon/static/media/cat-1.f4a79852.png"
+  },
+  {
+    "revision": "47ef05ecb014498342a95391bd179507",
+    "url": "/hackathon/static/media/cat-10.47ef05ec.png"
+  },
+  {
+    "revision": "feede63093053eeaae8e2b3611d230a2",
+    "url": "/hackathon/static/media/cat-12.feede630.png"
+  },
+  {
+    "revision": "7c2299b6d3af24abcf1c6db08c9108c1",
+    "url": "/hackathon/static/media/cat-14.7c2299b6.png"
+  },
+  {
+    "revision": "ee291ae4c19292584dbccc722340e5b4",
+    "url": "/hackathon/static/media/cat-16.ee291ae4.png"
+  },
+  {
+    "revision": "2f9fa1315da65ad4a7698eb036084576",
+    "url": "/hackathon/static/media/cat-5.2f9fa131.png"
+  },
+  {
+    "revision": "a402b31857b37e1a2f717fdaffe2116a",
+    "url": "/hackathon/static/media/cat-8.a402b318.png"
+  },
+  {
+    "revision": "a7368a2ab36887e1c2cf5eb636b1075d",
+    "url": "/hackathon/static/media/cat-11.a7368a2a.png"
+  },
+  {
+    "revision": "c0f2f644943a87a6bfbafbe97b80e199",
+    "url": "/hackathon/static/media/cat-13.c0f2f644.png"
+  },
+  {
+    "revision": "ecc0cbba849a2634beab9b59fef25b54",
+    "url": "/hackathon/static/media/cat-15.ecc0cbba.png"
+  },
+  {
+    "revision": "f863c27c24013a17fabef944b7fffc4f",
+    "url": "/hackathon/static/media/cat-2.f863c27c.png"
+  },
+  {
+    "revision": "d7e0366371164d7e95f084acef3814fd",
+    "url": "/hackathon/static/media/cat-4.d7e03663.png"
+  },
+  {
+    "revision": "8ce94f306d0a563e63ef0806cea47d4b",
+    "url": "/hackathon/static/media/cat-9.8ce94f30.png"
+  },
+  {
+    "revision": "e4431485c95f3c961503afa90fa593fe",
+    "url": "/hackathon/static/media/water-marker.e4431485.svg"
+  },
+  {
+    "revision": "d9db1d4246f08cc2aa943028cdc5db43",
+    "url": "/hackathon/static/media/nature-marker.d9db1d42.svg"
+  },
+  {
+    "revision": "c5fc4b7d394e5389816434eac70095b2",
+    "url": "/hackathon/static/media/energy-marker.c5fc4b7d.svg"
+  },
+  {
+    "revision": "7af20470f85bf1ad733bfeb8d0ce3037",
+    "url": "/hackathon/static/media/housing-marker.7af20470.svg"
+  },
+  {
+    "revision": "d0852ff6a5d1ad2ae86b341017931e70",
+    "url": "/hackathon/static/media/education-marker.d0852ff6.svg"
+  },
+  {
+    "revision": "c7baf21634546d6dbdd7a86a9c77a123",
+    "url": "/hackathon/index.html"
+  }
+];
